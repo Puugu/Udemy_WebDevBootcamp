3b9b37576163967668f1235deaa87fe8b6f78b1b
Puugu <therealckirkham@gmail.com>
2018-01-15 15:37:58 -0600

    update with comments and some more efficient code blocks after reviewing the solutions video

--- Section10-JavaScriptBasics_Functions/functionProblems.js
+++ Section10-JavaScriptBasics_Functions/functionProblems.js
@@ -1,32 +1,38 @@
 function isEven(num){
+	//check to make sure the argument is actually a number
 	if(typeof(num) !== 'number'){
 		return "ERROR: '"+num+"'' is not a number.";
 	}
-	else if (num%2 === 0){
-		return true;
-	} else{
-		return false;
+	//return true if even
+	//return false otherwise
+	else {
+		return num%2 === 0;  //This expression will either be true or false
 	}
 }
 
 function factorial(number){
+	//check to make sure the argument is actually a number
 	if(typeof(number) !== 'number'){
 		return "ERROR: '"+number+"'' is not a number.";
 	}
+	//check to see if the argument is negative
 	else if (number<0){
 		return "ERROR: '"+number+"'' is less than zero.";
 	}
+	//check to see if the argument is zero
 	else if (number===0){
 		return 1;
 	}
+	//calculate the factorial
 	else {
 		for(var i=(number-1); i>1;i--){
-			number=number*i;
+			number*=i;
 		}
 		return number;
 	}
 }
 
 function kebabToSnake(string){
+	//replace all instances of '-' in the string with '_'
 	return string.replace(/-/g, "_")
 }
