a780ed8e5c8a740fc15faebcc38d453768d51f99
Puugu <therealckirkham@gmail.com>
2018-01-15 09:05:11 -0600

    complete stalker exercise

---
+++ Section8-IntroductionToJavaScript/stalker.js
@@ -0,0 +1,8 @@
+//get user data and store in variables
+var fName = prompt("What is your first name?");
+var lName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+//output
+console.log("Your full name is "+ fName +" "+lName+".");
+console.log("You are "+age+" years old.");
