74b9377e6e745ae7632899bb33880150c4febd02
Puugu <therealckirkham@gmail.com>
2018-01-16 12:36:40 -0600

    update JavaScript to be more efficient after viewing solution video

--- Section11-JavaScriptBasics_Arrays/arrayProblems.js
+++ Section11-JavaScriptBasics_Arrays/arrayProblems.js
@@ -1,39 +1,38 @@
 //printReverse()
-function printReverse(array){
+function printReverse(arr){
 	//This function takes an array as an argument and prints its elements in the reverse order.
-	for(var i = array.length; i>=0; i--){
-		console.log(array[i]);
+	for(var i = arr.length-1; i>=0; i--){
+		console.log(arr[i]);
 	}
 }
 
 //isUniform()
-function isUniform(array){
+function isUniform(arr){
 	//This function takes an array as an argument and returns true if all the elements in the array are identical.
-	var value = array[0];
-	var isIdentical = true;
-	array.forEach(function(checkMe){
-		if(checkMe !== value){
-			isIdentical = false;
+	var value = arr[0];
+	for(var i=1; i<arr.length; i++){
+		if(arr[i] !== value){
+			return false;
 		}
-	})
-	return isIdentical;
+	}
+	return true;
 }
 
 //sumArray()
-function sumArray(array){
+function sumArray(arr){
 	//This function takes an array of numbers as an argument and returns the sum of all the numbers in the array.
 	var sum = 0;
-	array.forEach(function(value){
+	arr.forEach(function(value){
 		sum+=value;
 	})
 	return sum;
 }
 
 //max()
-function max(array){
+function max(arr){
 	//This function accepts an array of numbers and returns the largest value in the array. 
-	var maxVal = array[0];
-	array.forEach(function(value){
+	var maxVal = arr[0];
+	arr.forEach(function(value){
 		if(value>maxVal){
 			maxVal = value;
 		}
