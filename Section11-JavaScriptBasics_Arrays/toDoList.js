0101ed56e5c3cd2c5b32fa5a0fab6f87887c54b7
Puugu <therealckirkham@gmail.com>
2018-01-16 10:49:51 -0600

    complete unit for part 2 of the to do list code along

--- Section11-JavaScriptBasics_Arrays/toDoList.js
+++ Section11-JavaScriptBasics_Arrays/toDoList.js
@@ -1,4 +1,4 @@
-var toDos = ["Buy Groceries"];
+var toDos = ["Buy Groceries", "Sweep", "Kill Voldemort", "Clean David's office", "Buy Hogwarts textbooks"];
 
 //ask user what they want to do
 var input = prompt("What would you like to do?");
@@ -6,12 +6,13 @@ var input = prompt("What would you like to do?");
 while(input !== "quit"){
 	//check input
 	if (input === "list"){
-		console.log(toDos);
-		}
+		listToDos();
+	}
 	else if (input === "new"){
-		//ask for new todo
-		var newToDo=prompt("Enter new to do item.");
-		toDos.push(newToDo);
+		addToDo();
+	}
+	else if (input === "delete"){
+		deleteToDo();
 	}
 
 	//ask again for new input
@@ -19,3 +20,26 @@ while(input !== "quit"){
 
 }
 console.log("Ok, you have quit");
+
+function listToDos(){
+	console.log("**********");
+	toDos.forEach(function(toDoItem, index){
+		console.log(index+": "+toDoItem);
+	})
+	console.log("**********");
+}
+
+function addToDo(){
+	//ask for new todo
+		var newToDo=prompt("Enter new to do item.");
+		toDos.push(newToDo);
+		console.log("Added To Do Item");
+}
+
+function deleteToDo(){
+	//ask for index of to do item to be deleted
+	var deleteItem = prompt("Enter the index of the item to be deleted:");
+	//delete that todo
+	toDos.splice(deleteItem,1);
+	console.log("Deleted To Do Item");
+}
