6ccde2292bc50ce3452536e84c68ce7131b98ec1
Puugu <therealckirkham@gmail.com>
2018-01-18 20:39:55 -0600

    enable users to add new items to the to do list

--- Section18-TodoListProject/assets/js/todos.js
+++ Section18-TodoListProject/assets/js/todos.js
@@ -1,12 +1,24 @@
 //check off specific todos by clicking
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");	
 })
 
 //delete to do when clicking trashcan
-$(".deleteMe").on("click", function(event){
+$("ul").on("click", ".deleteMe", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+})
+
+//add new to-do item to the list
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//get text of new to do item
+		var toDoText = $(this).val();
+		//clear out the input
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span class=\"deleteMe\">X</span> "+toDoText+"</li>");
+	}
 })
